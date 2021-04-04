217ed51774693b280a4c8a12d9222a8a789d7fe4
SimhadriSirisha <sirishasimhadri5@gmail.com>
2021-04-04 13:50:49 +0530

    tail() implemented and tested

--- src/function.js
+++ src/function.js
@@ -2,5 +2,12 @@ module.exports = {
     head: (arrayEle) => {
         if(arrayEle.length == 0) return undefined;
         return arrayEle[0];
+    },
+    tail: (arr) => {
+        if(arr.length == 0) return undefined;
+        else{
+            arr.shift();
+            return arr;
+        }
     }
 }
--- src/function.spec.js
+++ src/function.spec.js
@@ -1,4 +1,4 @@
-let {head} = require('./function');
+let {head, tail} = require('./function');
 
 describe('head',()=>{
     it('head of an empty list is undefined',()=>{
@@ -6,5 +6,14 @@ describe('head',()=>{
     });
     it('head of a list is the first element',()=>{
         expect(head([1,2,3])).toEqual(1);
+    });
+})
+
+describe('tail',()=>{
+    it('tail of an empty list is undefined',()=>{
+        expect(tail([])).toEqual(undefined);
+    });
+    it('tail of a list return everything except first element',()=>{
+        expect(tail([1,2,3])).toEqual([2,3]);
     });
 })
